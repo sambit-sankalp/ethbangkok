78c694591d1c5e0b6b9f2676b9c00da307b091c1
Debrup-commits <debruppanda01@gmail.com>
2024-11-16 22:12:24 +0530

    Initializing Ai Agent and starter code

--- AiAgent/chatbot.py
+++ AiAgent/chatbot.py
@@ -31,6 +31,87 @@ required_attributes = [
     "max_gas_fee",
 ]
 
+
+# # Get all available tools
+# tools = cdp_toolkit.get_tools()
+
+# # Create the agent
+# agent_executor = create_react_agent(
+#     groq_chat,
+#     tools=tools,
+#     state_modifier="You are a helpful chatbot that can interact with the Sepolia blockchain using CDP AgentKit. You can create wallets and swap tokens"
+# )
+
+class InvokeContract(BaseModel):
+    """Input argument schema for contract invocation action."""
+
+    def call_invoke_contract(wallet: Wallet, source_chain: str) -> None:
+        """Invoke source chain contract using agent's wallet.
+            This invocation locks funds into the source chain address
+
+        Args:
+            wallet (Wallet): The wallet to sign the message from.
+
+        Returns:
+            None
+
+        """
+
+        invocation = wallet.invoke_contract(
+        contract_address=get_contract_address(source_chain), # source chain contract address
+        abi=get_contract_abi(),
+        method="deposit",
+        amount=transaction["data"]["amount"],
+        asset_id="eth"
+        )
+
+        invocation.wait()
+
+
+    def initialize_agent():
+        """Initialize the agent with CDP Agentkit."""
+        # TODO: Load the LLM model and CDP Agentkit values from the environment.
+
+        agentkit = CdpAgentkitWrapper(**values)
+
+        # Initialize CDP Agentkit Toolkit and get tools.
+        cdp_toolkit = CdpToolkit.from_cdp_agentkit_wrapper(agentkit)
+        tools = cdp_toolkit.get_tools()
+
+        # Define a new tool for signing messages.
+        invokeTool = CdpTool(
+        name="call_invoke_contract",
+        description=INVOKE_CONTRACT_PROMPT,
+        cdp_agentkit_wrapper=agentkit,
+        args_schema=SignMessageInput,
+        func=call_invoke_contract,
+        )
+
+        all_tools = tools.append(invokeTool)
+
+        # Store buffered conversation history in memory.
+        memory = MemorySaver()
+        config = {"configurable": {"thread_id": "CDP Agentkit Chatbot Example!"}}
+
+        # Create ReAct Agent using the LLM and CDP Agentkit tools.
+        return create_react_agent(
+            llm,
+            tools=all_tools,
+            checkpointer=memory,
+            state_modifier="You are a helpful chatbot that can interact on various chains using the Coinbase Developer Platform Agentkit. You have the power lock in user funds into the source chain contract to facilitate intent-based transactions.",
+        ), config
+
+    # Function to interact with the agent
+    def ask_agent(agent_executor, question: str):
+        for chunk in agent_executor.stream(
+            {"messages": [HumanMessage(content=question)]},
+            {"configurable": {"thread_id": "my_first_agent"}}
+        ):
+
+            # return agent response
+            return chunk["agent"]["messages"][0].content
+
+
 def get_missing_fields(data):
     """
     Determine which fields are missing from the transaction data.
