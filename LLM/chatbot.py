22e724a636e4f72e3ec19fc8a400bd91be93e17c
Akash Sahu <akashsahu006@gmail.com>
2024-11-16 21:22:59 +0530

    LLM Initialization

--- LLM/chatbot.py
+++ LLM/chatbot.py
@@ -17,32 +17,3 @@ conversational_memory_length = 5
 memory = ConversationBufferWindowMemory(
     k=conversational_memory_length, memory_key="chat_history", return_messages=True
 )
-
-
-# Helper function to check missing fields
-def get_missing_fields(data):
-    """
-    Determine which fields are missing from the transaction data.
-    Input should be a dictionary.
-    """
-    if not isinstance(data, dict):
-        raise ValueError("Input data must be a dictionary.")
-    return [key for key, value in data.items() if value == ""]
-
-
-# Generate chatbot response
-def generate_response(chat_history, user_input):
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            SystemMessage(content="You are a helpful bot."),
-            MessagesPlaceholder(variable_name="chat_history"),
-            HumanMessagePromptTemplate.from_template("{human_input}"),
-        ]
-    )
-    conversation = LLMChain(
-        llm=groq_chat,
-        prompt=prompt,
-        verbose=False,
-        memory=memory,
-    )
-    return conversation.predict(human_input=user_input)
