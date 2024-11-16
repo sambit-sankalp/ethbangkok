41b3947ec87f8bd74aa1226c633d25a09c4ae54f
Rituraj Tripsthy <rituraj.tripathy@gmail.com>
2024-11-16 20:26:53 +0700

    simple loader

--- Backend/Solver/solver.js
+++ Backend/Solver/solver.js
@@ -13,3 +13,42 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const db = admin.firestore();
+
+
+function loadingAnimation(duration = 5) {
+    const spinner = ['|', '/', '-', '\\'];
+    const totalSteps = duration * 10;
+    const delay = (duration * 1000) / totalSteps;
+  
+    let i = 0;
+  
+    const interval = setInterval(() => {
+      process.stdout.write(`\rLoading... ${spinner[i % spinner.length]}`);
+      i += 1;
+    }, delay);
+  
+    setTimeout(() => {
+      clearInterval(interval);
+      process.stdout.write('\rLoading complete!    \n');
+    }, duration * 1000);
+  }
+  
+  // Display introductory art
+  console.log('\n initiating');
+  
+  const art = `
+  
+  ██████╗░██╗░░██╗░█████╗░███████╗███╗░░██╗██╗██╗░░██╗███████╗██╗
+  ██╔══██╗██║░░██║██╔══██╗██╔════╝████╗░██║██║╚██╗██╔╝██╔════╝██║
+  ██████╔╝███████║██║░░██║█████╗░░██╔██╗██║██║░╚███╔╝░█████╗░░██║
+  ██╔═══╝░██╔══██║██║░░██║██╔══╝░░██║╚████║██║░██╔██╗░██╔══╝░░██║
+  ██║░░░░░██║░░██║╚█████╔╝███████╗██║░╚███║██║██╔╝╚██╗██║░░░░░██║
+  ╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚══════╝╚═╝░░╚══╝╚═╝╚═╝░░╚═╝╚═╝░░░░░╚═╝
+  `;
+  console.log(art);
+  
+  // Initialize readline for user input
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
